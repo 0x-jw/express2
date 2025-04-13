e0d1c164bfe935d2288b2f9bfbff29e3edeb1e35
0x-jw <jackwhyte@tutamail.com>
2025-04-13 23:15:36 +0100

    Update index.js

--- index.js
+++ index.js
@@ -11,4 +11,6 @@ app.get('/checkout', (req, res) => {
     res.json({'name': 'john smith', 'age': 12, 'nationallity': 'french'});
 })
 
-app.listen(3000);
+const port = process.env.PORT || 3000;
+
+app.listen(port);
